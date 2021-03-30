b5f7c4967e9d22f3f58b491ca7a012261126441c
Arpan Barua <arpanctg7@gmail.com>
2021-03-30 15:38:03 -0400

    db connected

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pz7qy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const productCollection = client.db("smartUser").collection("products");
-    console.log('database connected');
+  const productCollection = client.db("smartWatch").collection("products");
+    
 });
 
 app.listen(process.env.PORT || port)
