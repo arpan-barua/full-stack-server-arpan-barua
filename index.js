1ff4fc8120721932cbc29b58def2049ed2a9f9aa
Arpan Barua <arpanctg7@gmail.com>
2021-03-30 15:32:33 -0400

    db connection established

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const port = 5055;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pz7qy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productCollection = client.db("smartUser").collection("products");
+    console.log('database connected');
+});
+
+app.listen(process.env.PORT || port)
